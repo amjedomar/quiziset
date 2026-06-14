1b8f249bc4924fa4470b5d47dbd39897c49c6421
Amjed Omar <amjedomar201@gmail.com>
2026-06-14 23:58:40 +0300

    fix(frontend): fix next build command fails inside docker container

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,6 +1,14 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
+  /**
+   * set the "turbopack.root" to this project folder. without it next.js detects multiple
+   * npm lockfiles (this one + the root package-lock.json used for husky) and results in warning
+   * see https://stackoverflow.com/questions/79797750/next-js-inferred-the-wrong-workspace-root-error/79844648#79844648
+   */
+  turbopack: {
+    root: __dirname,
+  },
   sassOptions: {
     additionalData: '@use "@/theme.scss" as *;',
   },
