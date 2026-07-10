85cb501046c7e2c5b1d7ccb726a5044bd3975e75
Amjed Omar <amjedomar201@gmail.com>
2026-07-10 14:58:05 +0300

    test(frontend): fix failed QuizUpdateForm unit test in CI

--- frontend/src/components/quiz/quiz-update-form/quiz-update-form.spec.tsx
+++ frontend/src/components/quiz/quiz-update-form/quiz-update-form.spec.tsx
@@ -2,6 +2,10 @@ import { render } from '@testing-library/react'
 import { makeQuiz } from '@/test-utils/mocks'
 import { QuizUpdateForm } from './quiz-update-form'
 
+jest.mock('@/constants/api-url', () => ({
+  NEXT_PUBLIC_API_BASE_URL: 'http://mock-backend',
+}))
+
 const useGetSingleQuiz = jest.fn()
 jest.mock('@/generated-api-client/quiz', () => ({
   useGetSingleQuiz: (...args: unknown[]) => useGetSingleQuiz(...args),
