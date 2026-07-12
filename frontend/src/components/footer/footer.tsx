ed918562d4612a87f6023e7ab3154733d0bfdcbc
Amjed Omar <amjedomar201@gmail.com>
2026-07-12 16:27:44 +0300

    fix(frontend): improve footer

--- frontend/src/components/footer/footer.tsx
+++ frontend/src/components/footer/footer.tsx
@@ -1,6 +1,21 @@
+import { Link } from '@mui/joy'
+import styles from './footer.module.scss'
+
 export function Footer() {
   return (
-    // For now footer is just a spacer
-    <div style={{ padding: '32px 0' }} />
+    <div className={styles.footer}>
+      <p className={styles.note}>
+        Quiziset was developed by{' '}
+        <Link href="https://www.linkedin.com/in/amjedomar" target="_blank" fontSize="inherit">
+          Amjed Omar
+        </Link>
+      </p>
+      <p className={styles.note}>
+        Images from{' '}
+        <Link href="https://unsplash.com" target="_blank" underline="always" fontSize="inherit">
+          Unsplash
+        </Link>
+      </p>
+    </div>
   )
 }
